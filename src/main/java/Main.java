3d99eb9ec0a33fd4510786960a7c16561356c03d
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-08-04 17:07:38 +0200

    Main class created

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String vehicleRegistrationPlate = "PK1234";
+        int yearOfManufacture = 2018;
+        String driverFirstName = "Janusz";
+        String driverLastName = "Cebula";
+        String drivingLicenceNumber = "PL123456";
+    }
+}
