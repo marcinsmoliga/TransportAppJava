b3c0fbae8d4304b05f74af06500a385db699c6df
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-08-07 15:17:36 +0200

    Car class and Person class created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        String vehicleRegistrationPlate = "PK1234";
-        int yearOfManufacture = 2018;
-        String driverFirstName = "Janusz";
-        String driverLastName = "Cebula";
-        String drivingLicenceNumber = "PL123456";
+       Car car = new Car();
+       car.vehicleRegistrationPlate = "PK1234";
+       car.brand = "Volvo";
+
+       Person person = new Person();
+       person.firstName = "Janusz";
+       person.lastName = "Cebula";
+       person.birthYear = 1968;
+
+        System.out.println(car.brand + " " + car.vehicleRegistrationPlate);
+        System.out.println(person.firstName + " " + person.lastName + " " + person.birthYear);
     }
 }
