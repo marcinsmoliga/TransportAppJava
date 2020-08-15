eaf627619fae4242d137e99d506ae23b15a78c80
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-08-15 13:28:58 +0200

    CompanyApp class created

---
+++ src/main/java/com/example/app/CompanyApp.java
@@ -0,0 +1,9 @@
+package com.example.app;
+
+public class CompanyApp {
+
+	public static void main(String[] args) {
+	CompanyControl companyControl = new CompanyControl();
+		companyControl.printMenu();
+	}
+}
