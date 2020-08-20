315cc12c00360efa75b4b4a2431468e41dba25bf
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-08-20 15:51:57 +0200

    enumPackage deleted

--- src/main/java/com/example/app/CompanyControl.java
+++ src/main/java/com/example/app/CompanyControl.java
@@ -2,8 +2,6 @@ package com.example.app;
 
 import java.util.InputMismatchException;
 
-import com.example.enumPackage.CarType;
-import com.example.enumPackage.Option;
 import com.example.exception.NoSuchOptionException;
 import com.example.io.ConsolePrinter;
 import com.example.io.DataReader;
@@ -103,14 +101,14 @@ public class CompanyControl {
 	}
 
 	private void printOptions() {
-		for(Option option : Option.values()) {
+		for (Option option : Option.values()) {
 			consolePrinter.printNextLine(option.toString());
 		}
 		consolePrinter.printLine("Choose action: ");
 	}
 
 	private void printCarType() {
-		for(CarType carType : CarType.values()) {
+		for (CarType carType : CarType.values()) {
 			consolePrinter.printNextLine(carType.toString());
 		}
 		consolePrinter.printLine("Choose car type: ");
@@ -133,4 +131,62 @@ public class CompanyControl {
 		}
 		return carType;
 	}
+
+	public enum Option {
+		EXIT (0, "exit the program"),
+		ADD_CAR (1, "add car"),
+		ADD_PERSON (2, "add person"),
+		SHOW_CARS (3, "show all cars"),
+		SHOW_PEOPLE (4, "show all people");
+
+		private final int optionNumber;
+		private final String description;
+
+		Option(int optionNumber, String description) {
+			this.optionNumber = optionNumber;
+			this.description = description;
+		}
+
+		public static Option convertToOption(int number) throws NoSuchOptionException {
+			try {
+				return Option.values()[number];
+			} catch (ArrayIndexOutOfBoundsException e) {
+				throw new NoSuchOptionException("There is no such option as " + number);
+			}
+
+		}
+
+		@Override
+		public String toString() {
+			return optionNumber + " - " + description;
+		}
+	}
+
+	public enum CarType {
+		TRUCK(1, "add Truck"),
+		VAN(2, "add Van");
+
+		private final int carNumber;
+		private final String description;
+
+		CarType(int carNumber, String description) {
+			this.carNumber = carNumber;
+			this.description = description;
+		}
+
+		public static CarType convertToCarType(int number) throws NoSuchOptionException {
+			try {
+				return CarType.values()[number - 1];
+			} catch (ArrayIndexOutOfBoundsException e) {
+				throw new NoSuchOptionException("There is no such option as " + number);
+			}
+		}
+
+		@Override
+		public String toString() {
+			return "(" + carNumber + ")" + " - " + description;
+		}
+	}
+
+
 }
